33a019d55b0d3d9a74c50b94aa5b92395dbef87e
Nilan Saha <nilan@aaqua.live>
2021-10-08 23:30:21 -0700

    Added TreePlayerV2 - No weakness

--- play.py
+++ play.py
@@ -1,6 +1,6 @@
 from game import Game
 from tictactoe import TicTacToe
-from players import RandomPlayer, HumanPlayer, TreePlayerV1, TreePlayerV2, TreePlayerV3
+from players import RandomPlayer, HumanPlayer, TreePlayerV1, TreePlayerV2
 
 
 # env = TicTacToe(5, 4)
@@ -30,23 +30,48 @@ from players import RandomPlayer, HumanPlayer, TreePlayerV1, TreePlayerV2, TreeP
 # game = Game(env, player1, player2, verbose=True)
 # game.play()
 
-env = TicTacToe(3, 3)
-player1 = TreePlayerV1(env)
-player2 = HumanPlayer(env)
+# env = TicTacToe(3, 3)
+# player1 = TreePlayerV1(env)
+# player2 = HumanPlayer(env)
 
-game = Game(env, player1, player2, verbose=True)
-game.play()
+# game = Game(env, player1, player2, verbose=True)
+# game.play()
 
+loss_envs = []
+win_stats = {-1: 0, 0: 0, 1: 0}
 
-# win_stats = {-1: 0, 0: 0, 1: 0}
+for _ in range(1000):
+    env = TicTacToe(3, 3)
+    player1 = TreePlayerV1(env)
+    player2 = RandomPlayer(env)
 
-# for _ in range(100):
-#     env = TicTacToe(3, 3)
-#     player1 = TreePlayerV1(env)
-#     player2 = RandomPlayer(env)
+    game = Game(env, player1, player2, verbose=True)
+    game.play()
+    win_stats[game.winner] += 1
 
-#     game = Game(env, player1, player2, verbose=True)
-#     game.play()
-#     win_stats[game.winner] += 1
+    if game.winner == 1:
+        loss_envs.append(env)
 
-# print(win_stats)
+print("TreePlayerV1 vs RandomPlayer", win_stats)
+
+# for env in loss_envs:
+#     env.render()
+#     print(env.move_history)
+#     print()
+
+loss_envs = []
+win_stats = {-1: 0, 0: 0, 1: 0}
+
+for _ in range(1000):
+    env = TicTacToe(3, 3)
+    player1 = TreePlayerV2(env)
+    player2 = RandomPlayer(env)
+
+    game = Game(env, player1, player2, verbose=True)
+    game.play()
+    win_stats[game.winner] += 1
+
+    if game.winner == 1:
+        loss_envs.append(env)
+
+print("TreePlayerV2 vs RandomPlayer", win_stats)
--- players.py
+++ players.py
@@ -56,9 +56,23 @@ class TreePlayerV1(Player):
     inherits the sum of the points of their children and it gets propagated to the top
 
     Remark: Does pretty well in a 3 X 3. Against a human always draws unless the other player
-    makes a mistake. Has over 90% win ratio with a random player so pretty good. But obviously
+    makes a mistake. Has great stats with a random player so pretty good. But obviously
     extremely slow and not scalable if the grid is bigger and that is why we need algorithms
     that can generalize and not memorize
+
+    Upon further inspection there is definitely weakness in it because sometimes it chooses
+    not to take the winning move and makes a move that will increase its chances of winning in
+    the next move. Which makes sense because of how we are awarding points.
+    Let us assume there are two nodes on a level - one of them is a terminal node and that move
+    grants win so it gets 1 point. There is another node which is not a terminal node but only
+    one of its children wins the game so in turn this particular node also gets awarded 1 point
+    because the point gets propagated to it. Adding a notion of points based on faster
+    wins should definitely solve this. One way of solving this is also by normalizing the
+    propagated score to the parent by dividing with the number of children. Terminal nodes
+    can one complete point but non-terminal nodes on the same level is less likely to get 1 point
+    as well
+
+    Stats on a game with RandomPlayer = {Draw: 149, TreePlayerV1: 826, RandomPlayer: 25}
     """
 
     model = None
@@ -69,7 +83,7 @@ class TreePlayerV1(Player):
             model = load_model("tree_player_v1")
             if not model:
                 model = self._generate_game_tree()
-                save_model(self.model, "tree_player_v1")
+                save_model(model, "tree_player_v1")
             TreePlayerV1.model = model
 
     def _generate_game_tree(self):
@@ -102,6 +116,69 @@ class TreePlayerV1(Player):
         sorted_moves = sorted(
             node.children.items(), key=lambda x: x[1].points, reverse=True
         )
-        print([(move, node.points) for move, node in sorted_moves])
+        # print([(move, node.points) for move, node in sorted_moves])
+        best_move = sorted_moves[0][0]
+        return best_move
+
+
+class TreePlayerV2(Player):
+    """
+    The player generates the entire game tree and gives 1 point to each terminal
+    node(move) that results in a win for player1 (0). Every parent node
+    inherits the sum of the points of their children normalized by the number of children
+    it has. It gets propagated to the top
+
+    Remark: Does extremely well in a 3 X 3. Against a human always draws unless the other player
+    makes a mistake. Has insanely good stats with the random player. But obviously
+    extremely slow and not scalable if the grid is bigger and that is why we need algorithms
+    that can generalize and not memorize
+
+    Remarks: Does not have any weakness at all
+
+    Stats on a game with RandomPlayer = {Draw: 6, TreePlayerV2: 994, RandomPlayer: 0}
+    """
+
+    model = None
+
+    def __init__(self, env):
+        super().__init__(env)
+        if not TreePlayerV2.model:
+            model = load_model("tree_player_v2")
+            if not model:
+                model = self._generate_game_tree()
+                save_model(model, "tree_player_v2")
+            TreePlayerV2.model = model
+
+    def _generate_game_tree(self):
+        def helper(node, env):
+            moves = env.available_moves()
+            for move in moves:
+                child_node = Node(move)
+                frozen_env = deepcopy(env)
+                done, winner = frozen_env.move(move)
+                node.children[move] = child_node
+                if not done:
+                    helper(child_node, frozen_env)
+                else:
+                    if winner == 0:
+                        child_node.points = 1
+
+            total_child_points = sum([child.points for child in node.children.values()])
+            node.points = total_child_points / len(node.children)
+
+            return node
+
+        node = Node(-1)
+        return helper(node, self.env)
+
+    def get_move(self):
+        node = self.model
+        for move in self.env.move_history:
+            node = node.children[move]
+
+        sorted_moves = sorted(
+            node.children.items(), key=lambda x: x[1].points, reverse=True
+        )
+        # print([(move, node.points) for move, node in sorted_moves])
         best_move = sorted_moves[0][0]
         return best_move
